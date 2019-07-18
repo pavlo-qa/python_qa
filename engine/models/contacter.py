82a419408c75ed11995afeca48c6776898f6e987
pavlo-qa <pavlo@detesters.nl>
2019-07-18 15:56:44 +0200

    Added python packages
    Engine files separated into helpers and models

---
+++ engine/models/contacter.py
@@ -0,0 +1,10 @@
+
+
+class Contacter:
+
+    def __init__(self, email, topic, description, name="Pavlo", surname="Chigishev"):
+        self.name = name
+        self.surname = surname
+        self.email = email
+        self.topic = topic
+        self.description = description
--- tests/test_contact_form.py
+++ tests/test_contact_form.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from engine.application import Application
+from engine.helpers.application import Application
 import pytest
 
 
